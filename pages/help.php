eb126e59e0a98a76b8d4ef06f9c5b13ef919e1b6
Michael Risher <rishermichael@gmail.com>
2017-07-06 13:18:12 -0700

    Cleaned up the download to be more precise

--- pages/help.php
+++ pages/help.php
@@ -4,17 +4,28 @@
 	 * User: Michael Risher
 	 * Date: 7/5/2017
 	 * Time: 13:57
+	 * http://www.media-division.com/php-download-script-with-resume-option
 	 */
-	$file = CORE_PATH . 'assets/misc/help.docx';
 
-	header('Content-Description: File Transfer');
+	//turn off compression
+	@apache_setenv( 'no-gzip', 1 );
+	@ini_set( 'zlib.output_compression', 'Off' );
+
+	$file = CORE_PATH . 'assets/misc/help.docx';
+	if ( !file_exists( $file ) ) {
+		header( "HTTP/1.0 400 Bad Request" );
+		exit;
+	}
+
+
+
 //	header('Content-Type: application/octet-stream');
+	header('Pragma: public');
+	header('Expires: -1');
+	header('Cache-Control: public, must-revalidate, post-check=0, pre-check=0');
 	header('Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document');
 	header('Content-Disposition: attachment; filename="help.docx"');
-	header('Content-Transfer-Encoding: binary');
-	header('Expires: 0');
-	header('Cache-Control: must-revalidate');
-	header('Pragma: public');
 	header('Content-Length: ' . filesize($file));
 	readfile( $file );
+	ob_flush();
 	ob_clean();
